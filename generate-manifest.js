00ae8f45a22f503eab2c6f4584543afedc18cfe5
ShaneLund <153680191+ShaneLund@users.noreply.github.com>
2026-03-15 01:09:24 -0400

    Update generate-manifest.js

--- generate-manifest.js
+++ generate-manifest.js
@@ -5,7 +5,7 @@ let manifest = fs.readFileSync('fxmanifest.lua', 'utf8');
 
 manifest = manifest
   .replace(/version '.*?'/, `version '${pkg.version}'`)
-  .replace(/description '.*?'/, `description '${pkg.description || 'AAA TypeScript Boilerplate for Mercy Framework (React NUI + tRPC)'}'`)
+  .replace(/description '.*?'/, `description '${pkg.description || 'Boilerplate for Mercy Framework (React NUI + tRPC)'}'`)
   .replace(/author '.*?'/, `author '${pkg.author || 'Mercy Collective + You'}'`);
 
 fs.writeFileSync('fxmanifest.lua', manifest);
